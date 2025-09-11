8fa7cc995a428f58a052101c60cef323649a0b3f
Alvandi Immanuille Saerang <alvandisaerang.gmail.com>
2025-09-12 00:29:52 +0700

    Penyesuaian db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,5 +1,5 @@
 <?php
-$host = '145.79.14.134';
+$host = '153.92.15.82';
 $dbname = 'u199348039_aisight_db';
 $username = 'u199348039_admin'; // Ganti dengan username dari Database Management
 $password = '@!$Db415'; // Ganti dengan password dari Database Management
